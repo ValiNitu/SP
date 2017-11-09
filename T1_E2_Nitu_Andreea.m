d7ad84410f64516f8052f6bb5999ad5167229637
ValiNitu <32988417+ValiNitu@users.noreply.github.com>
2017-11-09 14:57:29 +0200

    Delete T1_E2_Nitu_Andreea.m

--- T1_E2_Nitu_Andreea.m
+++
@@ -1,6 +0,0 @@
-a=randn(1,10) %vectorul generat cu elemente aleatoare cu distributie normala de o linie si 10 coloane
-for i=1:1:10 % parcurg vectorul
- if a(i)<0 % daca elementul de pe pozitia i este negativ, se afiseaza 
-display (a(i))
-end
-end

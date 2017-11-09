dd799ee8251e1608e4a611a2d6224fa567d98822
ValiNitu <32988417+ValiNitu@users.noreply.github.com>
2017-11-09 14:54:51 +0200

    Delete T1_E1_Nitu_Andreea.m

--- T1_E1_Nitu_Andreea.m
+++
@@ -1,5 +0,0 @@
-% b trebuie sa aiba lungimea 21 pentru a avea loc inmultirea
-a=0:0.1:2 %vectorul a incepe de la 0 si se termina la 2 iar pasul este de 0.1
-b=ones(21,1) %b este o matrice cu toate elementele egale cu 1 de 21 de linii si o coloana
-c=a*b
-d=b*a
